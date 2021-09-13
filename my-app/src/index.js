3024d31a320cd6a35b4f731e8d4087a62ec23d67
Marcin Wirzeski <mwirzeski@future-processing.com>
2021-09-13 16:00:00 +0200

    the-github-cards-app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,64 @@
-import React, { useState } from 'react';
+import React, { } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+{name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
 
-function Button() {
-    const [counter, setCounter] = useState(5);
-    return <button onClick={() => setCounter(counter*2)}>{counter}</button>;
+const CardList = (props) => ( 
+    <div>
+        {props.profiles.map(profile => <Card {...profile}/>)}
+    </div>
+);
+
+class Card extends React.Component {
+    render() {
+        const profile = this.props;
+        return (
+            <div className="github-profile">
+            <img src={profile.avatar_url} alt="alternative" />
+          <div className="info">
+            <div className="name">{profile.name}</div>
+            <div className="company">{profile.company}</div>
+          </div>
+          </div>
+
+        );
+    }
+}
+
+class Form extends React.Component {
+    render() {
+        return (
+            <form action="">
+                <input type="text" placeholder="GutHub username"/>
+                <button>Add card</button>
+            </form>
+        );
+    }
+}
+
+class App extends React.Component {
+    state = {
+            profiles: testData,
+        };
+    
+    render() {
+        return (
+            <div>
+                <div className="header">{this.props.title}</div>
+                <Form />
+                <CardList profiles={this.state.profiles} />
+            </div>
+
+        );
+    }
 }
 
 ReactDOM.render(
-    <Button />,
+    <App title="The GutHub Cards App"/>,
     document.getElementById('root')
-  );  
+  );
