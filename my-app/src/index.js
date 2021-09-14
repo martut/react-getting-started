4b2ac04d39c66196f57c9b1d94b2c47242ba4354
Marcin Wirzeski <martut2762@gmail.com>
2021-09-14 15:59:28 +0200

    star-match-game-part1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,78 +1,143 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios'
 import './index.css';
 
+// STAR MATCH - Starting Template
+const StarsDisplay = props => (
+  <>
+    {utils.range(1, props.count).map(starId => 
+        <div key={starId} className="star" />
+        )}
+    </>
+);
 
-const CardList = (props) => ( 
-    <div>
-        {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
+const PlayAgain = props => (
+    <div className="game-done">
+        <button onClick={props.onClick} >Play Again</button>
     </div>
 );
 
-class Card extends React.Component {
-    render() {
-        const profile = this.props;
-        return (
-            <div className="github-profile">
-            <img src={profile.avatar_url} alt="alternative" />
-          <div className="info">
-            <div className="name">{profile.name}</div>
-            <div className="company">{profile.company}</div>
+const PlayNumber = props => (
+    <button className="number"
+    style={{ backgroundColor: colors[props.status]}} 
+    onClick={() => props.onClick(props.number, props.status)}>
+        {props.number}
+        </button>
+);
+
+const StarMatch = () => {
+    const [stars, setStars] = useState(utils.random(1,9));
+    const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+    const [candidateNums, setCandidateNums] = useState([]);
+
+    const candidatesAreWrong = utils.sum(candidateNums) > stars;
+    const gameIsDone = availableNums.length === 0;
+
+    const resetGame = () => {
+        setStars(utils.random(1, 9));
+        setAvailableNums(utils.range(1, 9));
+        setCandidateNums([]);
+    }
+
+    const numberStatus = (number) => {
+        if (!availableNums.includes(number)) {
+            return 'used';
+        }
+        if (candidateNums.includes(number)) {
+            return candidatesAreWrong ? 'wrong': 'candidate';
+            
+        }
+        return 'available';
+    }
+
+
+    const onNumberClick = (number, currentStatus) => {
+         if (currentStatus === 'used') {
+             return;
+         }
+         const newCandidateNums = 
+            currentStatus === 'available'         
+                ? candidateNums.concat(number)
+                : candidateNums.filter(c => c !== number);
+         if (utils.sum(newCandidateNums) !== stars) {
+             setCandidateNums(newCandidateNums);
+         } else {
+             const newAvailableNums = availableNums.filter(
+                 n => !newCandidateNums.includes(n)
+             );
+             setStars(utils.randomSumIn(newAvailableNums, 9));
+             setAvailableNums(newAvailableNums);
+             setCandidateNums([]);
+         }
+    }
+
+    return (
+      <div className="game">
+        <div className="help">
+          Pick 1 or more numbers that sum to the number of stars
+        </div>
+        <div className="body">
+          <div className="left">
+              {gameIsDone ? (
+                  <PlayAgain onClick={resetGame} />
+              ) :
+              (
+                <StarsDisplay count={stars}/>
+              )}
           </div>
+          <div className="right">
+              {utils.range(1, 9).map(number => 
+                <PlayNumber 
+                    key={number}
+                    number={number}
+                    status={numberStatus(number)}
+                    onClick={onNumberClick} />
+                )}
           </div>
+        </div>
+        <div className="timer">Time Remaining: 10</div>
+      </div>
+    );
+  };
+  
+  // Color Theme
+ const colors = {
+   available: 'lightgray',
+    used: 'lightgreen',
+    wrong: 'lightcoral',
+    candidate: 'deepskyblue',
+  };
+  
+  // Math science
+  const utils = {
+    // Sum an array
+    sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+  
+    // create an array of numbers between min and max (edges included)
+    range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+  
+    // pick a random number between min and max (edges included)
+    random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+  
+    // Given an array of numbers and a max...
+    // Pick a random sum (< max) from the set of all available sums in arr
+    randomSumIn: (arr, max) => {
+      const sets = [[]];
+      const sums = [];
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = 0, len = sets.length; j < len; j++) {
+          const candidateSet = sets[j].concat(arr[i]);
+          const candidateSum = utils.sum(candidateSet);
+          if (candidateSum <= max) {
+            sets.push(candidateSet);
+            sums.push(candidateSum);
+          }
+        }
+      }
+      return sums[utils.random(0, sums.length - 1)];
+    },
+  };
+  
+  ReactDOM.render(<StarMatch />, document.getElementById('root'));
+  
 
-        );
-    }
-}
-
-class Form extends React.Component {
-    state = { userName: ''};
-    handleSubmit = async (event) => {
-        event.preventDefault();
-        const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`)
-        this.props.onSubmit(resp.data)
-        this.setState({userName: ''});
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" 
-                    placeholder="GutHub username" 
-                    required
-                    value={this.state.userName}
-                    onChange={event => this.setState({userName: event.target.value})}
-                />
-                <button>Add card</button>
-            </form>
-        );
-    }
-}
-
-class App extends React.Component {
-    state = {
-            profiles: [],
-        };
-    addNewProfile = (profileData) => {
-        this.setState(prevState => ({
-            profiles: [...prevState.profiles, profileData],
-        }))
-    };
-
-    render() {
-        return (
-            <div>
-                <div className="header">{this.props.title}</div>
-                <Form onSubmit={this.addNewProfile} />
-                <CardList profiles={this.state.profiles} />
-            </div>
-
-        );
-    }
-}
-
-ReactDOM.render(
-    <App title="The GutHub Cards App"/>,
-    document.getElementById('root')
-  );
